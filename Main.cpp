c630b43fee38d9c11a2711f7b760c96f2c3f2a84
eullernunes <elrnunes@hotmail.com>
2024-03-28 14:42:22 -0300

    atualizando

--- Main.cpp
+++ Main.cpp
@@ -1,36 +1,61 @@
 #include "Grafo.h"
-#include<iostream>
-#include<chrono>
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 
 
 
 using namespace std;
 using namespace std::chrono;
 
+void processaArquivoCSV(const std::string &arquivo)
+{
+    ifstream file(arquivo);
+    string line;
 
+    // Ignora as duas primeiras linhas com os comentários
+    getline(file, line);
+    int V = stoi(line.substr(line.find(':') + 1).c_str());
+
+    Grafo grafo(V);
+
+    getline(file, line);
+
+    // Lê as arestas
+    while (getline(file, line))
+    {
+        replace(line.begin(), line.end(), ',', ' ');
+        istringstream iss(line);
+        int v, w;
+        if (iss >> v >> w)
+        {
+            grafo.addAresta(v, w);
+        }
+    }
+
+    file.close();
+
+    auto inicioDfs = high_resolution_clock::now();
+    grafo.encontrarCiclosDfs();
+    auto fimDFS = high_resolution_clock::now();
+    auto duracaoDFS = duration_cast<microseconds>(fimDFS - inicioDfs);
+    cout << "Tempo de execução DFS: " << duracaoDFS.count() << " microssegundos" << endl;
+
+    // Medindo o tempo de execução da Permutação
+    auto inicioPermutacao = high_resolution_clock::now();
+    grafo.encontrarCiclosPorPermutacao();
+    auto fimPermutacao = high_resolution_clock::now();
+    auto duracaoPermutacao = duration_cast<microseconds>(fimPermutacao - inicioPermutacao);
+    cout << "Tempo de execução Permutação: " << duracaoPermutacao.count() << " microssegundos" << endl;
+}
 
 int main()
 {
-	Grafo g(5); // Criando um grafo com 5 vértices
-	g.addAresta(0, 1);
-	g.addAresta(1, 2);
-	g.addAresta(2, 0);
-	g.addAresta(1, 3);
-	g.addAresta(3, 4);
-	g.addAresta(4, 1);
 
-	auto inicioDfs = high_resolution_clock::now();
-	g.encontrarCiclosDfs();
-	auto fimDFS = high_resolution_clock::now();
-	auto duracaoDFS = duration_cast<microseconds>(fimDFS - inicioDfs);
-	cout << "Tempo de execução DFS: " << duracaoDFS.count() << " microssegundos" << endl;
-
-	// Medindo o tempo de execução da Permutação
-	auto inicioPermutacao = high_resolution_clock::now();
-	g.encontrarCiclosPorPermutacao();
-	auto fimPermutacao = high_resolution_clock::now();
-	auto duracaoPermutacao = duration_cast<microseconds>(fimPermutacao - inicioPermutacao);
-	cout << "Tempo de execução Permutação: " << duracaoPermutacao.count() << " microssegundos" << endl;
+    processaArquivoCSV("grafo1_5.csv");
 
 	return 0;
 }
